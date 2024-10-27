7abf51bd4fdfade32c4e151ede255ab56238a99e
_SportDay_ <49455066+SportDay@users.noreply.github.com>
2024-10-27 14:52:11 +0100

    add timeout simplify code

--- src/main/java/fr/esiee/app/utils/CompileAndExecUtils.java
+++ src/main/java/fr/esiee/app/utils/CompileAndExecUtils.java
@@ -1,115 +1,140 @@
 package fr.esiee.app.utils;
 
+import fr.esiee.app.exception.RestApiException;
 import org.apache.commons.lang3.SystemUtils;
 
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaFileObject;
 import javax.tools.ToolProvider;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CompileAndExecUtils {
 
+  public static final int EXEC_TIMEOUT_SEC = 5;
+
   public enum Operation {
     COMPILE, EXECUTE
   }
 
-  public static String processText(String code, Operation operation) throws IOException, InterruptedException {
+  public static String processText(String code, Operation operation)
+          throws IOException, InterruptedException, ExecutionException {
     Objects.requireNonNull(code);
-    var className = extractClassName(code);
-    if (className.isEmpty()) {
+    var classNameOpt = extractClassName(code);
+    if (classNameOpt.isEmpty()) {
       throw new IllegalStateException("no class name could be extracted");
     }
-    return switch (operation) {
-      case COMPILE -> compileJavaClass(code, className.get());
-      case EXECUTE -> compileAndExecuteJavaCode(code, className.get());
-    };
-  }
 
-  private static String compileJavaClass(String javaCode, String className) throws IOException {
-    Objects.requireNonNull(javaCode);
-    Objects.requireNonNull(className);
+    var className = classNameOpt.get();
     var targetDir = createTempPath();
     var javaFilePath = targetDir.resolve(className + ".java");
     var classFilePath = targetDir.resolve(className + ".class");
 
     try {
-      writeToFile(javaCode, javaFilePath);
-      return compileJavaFile(javaFilePath);
+      return switch (operation) {
+        case COMPILE -> compileJavaClass(code, className, javaFilePath);
+        case EXECUTE -> compileAndExecuteJavaCode(code, className, targetDir, javaFilePath, classFilePath);
+      };
     } finally {
       deleteFile(javaFilePath);
       deleteFile(classFilePath);
     }
   }
 
-  private static String compileAndExecuteJavaCode(String javaCode, String className) throws IOException, InterruptedException {
+  private static String compileJavaClass(String javaCode, String className, Path javaFilePath) throws IOException {
     Objects.requireNonNull(javaCode);
     Objects.requireNonNull(className);
-    var targetDir = createTempPath();
-    var javaFilePath = targetDir.resolve(className + ".java");
-    var classFilePath = targetDir.resolve(className + ".class");
+    Objects.requireNonNull(javaFilePath);
 
-    try {
-      writeToFile(javaCode, javaFilePath);
-      var compileErrors = compileJavaFile(javaFilePath);
-      if (!compileErrors.isEmpty()) {
-        return compileErrors;
-      }
-      if(isPosixFamily()) {
-        addExecutePermission(classFilePath);
-      }
-      return executeClassFile(targetDir, className);
-    } finally {
-      deleteFile(classFilePath);
-      deleteFile(javaFilePath);
+    writeToFile(javaCode, javaFilePath);
+    return compileJavaFile(javaFilePath);
+  }
+
+  private static String compileAndExecuteJavaCode(String javaCode, String className, Path targetDir, Path javaFilePath, Path classFilePath)
+          throws IOException, InterruptedException, ExecutionException {
+    Objects.requireNonNull(javaCode);
+    Objects.requireNonNull(className);
+
+    writeToFile(javaCode, javaFilePath);
+    var compileErrors = compileJavaFile(javaFilePath);
+    if (!compileErrors.isEmpty()) {
+      return compileErrors;
     }
+    if(isPosixFamily()) {
+      addExecutePermission(classFilePath);
+    }
+    return executeClassFile(targetDir, className);
   }
 
   private static boolean isPosixFamily() {
     return SystemUtils.IS_OS_UNIX || SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC;
   }
 
-  private static String executeClassFile(Path classDirectory, String className) throws IOException, InterruptedException {
+  private static String executeClassFile(Path classDirectory, String className)
+          throws IOException, InterruptedException, ExecutionException {
     Objects.requireNonNull(classDirectory);
     Objects.requireNonNull(className);
-    var output = "";
-    try (var outputStream = new ByteArrayOutputStream();
-         var printStream = new PrintStream(outputStream, true, StandardCharsets.UTF_8)) {
-      var processBuilder = new ProcessBuilder("java", "-cp", classDirectory.toString(), className);
-      var process = processBuilder.redirectErrorStream(true).start();
-      try (var processOutput = process.getInputStream()) {
-        processOutput.transferTo(printStream);
+
+
+    var process = new ProcessBuilder("java", "-cp", classDirectory.toString(), className)
+            .redirectErrorStream(true)
+            .start();
+
+    try (var executor = Executors.newSingleThreadExecutor();
+         ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+      var outputFuture = executor.submit(() -> {
+        try (var processOutput = process.getInputStream()) {
+          processOutput.transferTo(outputStream);
+        } catch (IOException e) {
+          throw new RestApiException(e);
+        }
+      });
+
+      var finished = process.waitFor(EXEC_TIMEOUT_SEC, TimeUnit.SECONDS);
+
+      if (!finished) {
+        process.destroy();
       }
-      process.waitFor();
-      output = outputStream.toString(StandardCharsets.UTF_8);
+
+      outputFuture.get();
+
+      var output = outputStream.toString(StandardCharsets.UTF_8);
+      if (!finished){
+        return output + "\nExecution timed out after " + EXEC_TIMEOUT_SEC + " seconds.\n";
+      }
+      return output;
     }
-    return output;
   }
 
   private static String compileJavaFile(Path javaFilePath) {
     Objects.requireNonNull(javaFilePath);
     var compiler = ToolProvider.getSystemJavaCompiler();
     var diagnostics = new DiagnosticCollector<JavaFileObject>();
-    var fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+    var fileManager = compiler.getStandardFileManager(diagnostics, Locale.ROOT, StandardCharsets.UTF_8);
     var compilationUnits = fileManager.getJavaFileObjects(javaFilePath);
-    var success = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits)
-            .call();
+    var success = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits).call();
     var errors = new ArrayList<String>();
 
     if (!success) {
       for (var diagnostic : diagnostics.getDiagnostics()) {
         var error = String.format("Error on line %d in %s%n%s",
                 diagnostic.getLineNumber(),
-                diagnostic.getSource().toUri(),
-                diagnostic.getMessage(null));
+                diagnostic.getSource().getName(),
+                diagnostic.getMessage(Locale.ROOT));
         errors.add(error);
       }
     }
