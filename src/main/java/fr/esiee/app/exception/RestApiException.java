08232d9faf205fd9ce7629e9ec88e26ad00302cc
_SportDay_ <49455066+SportDay@users.noreply.github.com>
2024-11-01 20:28:13 +0100

    remove unused imports

--- src/main/java/fr/esiee/app/exception/RestApiException.java
+++ src/main/java/fr/esiee/app/exception/RestApiException.java
@@ -1,6 +1,12 @@
 package fr.esiee.app.exception;
 
+import java.io.Serial;
+
 public class RestApiException extends RuntimeException {
+
+  @Serial
+  private static final long serialVersionUID = -5741898349056214368L;
+
   public RestApiException(String message) {
     super(message);
   }
